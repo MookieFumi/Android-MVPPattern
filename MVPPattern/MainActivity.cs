0ac44ebd588b9318588d70237be1fda2787bc9d5
Miguel Angel Martin Hrdez <miguel.martin@analyticalways.com>
2017-12-01 08:32:49 +0100

    Initial commit

--- MVPPattern/MainActivity.cs
+++ MVPPattern/MainActivity.cs
@@ -1,6 +1,8 @@
-﻿using Android.App;
-using Android.Widget;
+﻿using System.Threading.Tasks;
+using Android.App;
 using Android.OS;
+using Android.Views;
+using Android.Widget;
 
 namespace MVPPattern
 {
@@ -10,9 +12,20 @@ namespace MVPPattern
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            var textView = FindViewById<TextView>(Resource.Id.textView);
+            var button = FindViewById<Button>(Resource.Id.button);
+            var progressBar = FindViewById<ProgressBar>(Resource.Id.progressBar);
+
+
+            button.Click += async (sender, args) =>
+            {
+                progressBar.Visibility = ViewStates.Visible;
+                await Task.Delay(1500);
+                textView.Text = new QuotesService().GetRandomQuoute();
+                progressBar.Visibility = ViewStates.Gone;
+            };
         }
     }
 }
---
+++ MVPPattern/QuotesService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MVPPattern
+{
+    public interface IQuotesService
+    {
+        string GetRandomQuoute();
+    }
+
+    public class QuotesService : IQuotesService
+    {
+        public string GetRandomQuoute()
+        {
+            var items = GetQuoutes();
+            var random = new Random();
+            var index = random.Next(items.Count());
+
+            return items.ElementAt(index);
+        }
+
+        private static IEnumerable<string> GetQuoutes()
+        {
+            return new List<string>
+            {
+                "Be yourself. everyone else is already taken.",
+                "A room without books is like a body without a soul.",
+                "You only live once, but if you do it right, once is enough.",
+                "Be the change that you wish to see in the world.",
+                "If you tell the truth, you don't have to remember anything."
+            };
+        }
+    }
+}
